628a01e22feffa26eaf420c6b9a36a72d4b1105c
lukmanjpg <spontycsgoonelove@gmail.com>
2021-08-13 10:28:45 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const multer = require('multer');
+
+const PORT = process.env.PORT || 3000;
+const storage = multer.diskStorage({
+  	destination: function (req, file, cb) {
+   	 	cb(null, './public/');
+  	},
+  	filename: function (req, file, cb) {
+  		const fileFormat = file.mimetype.split('/');
+        cb(null, file.originalname);
+  	}
+});
+
+
+const upload = multer({ storage: storage })
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+	console.log('GOOD');
+});
+
+app.post('/img', upload.single('img'), (req, res) => {
+	res.json({img: __dirname});
+});
